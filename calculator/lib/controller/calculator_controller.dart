6439b92db8118ae26b53baf0b878ecbd0343d574
kopro-ov <koprodev@gmail.com>
2022-05-29 15:41:22 +0900

    opearand 메소드 작업

--- calculator/lib/controller/calculator_controller.dart
+++ calculator/lib/controller/calculator_controller.dart
@@ -1,26 +1,26 @@
 import 'package:get/state_manager.dart';
 
 class CalculatorController extends GetxController {
-  var num1 = '0'.obs;
-  var num2 = '0'.obs;
+  var ouput = '0'.obs;
   var result = '0'.obs;
+  var operation = ''.obs;
 
   reset() {
-    num1.value = '0';
-    num2.value = '0';
+    ouput.value = '0';
     result.value = '0';
+    operation.value = '';
   }
 
   operand(String o) {
-    int x = int.parse(num1.value);
-    int y = int.parse(num2.value);
+    double x = double.parse(result.value);
+    double y = double.parse(ouput.value);
 
     switch (o) {
       case 'AC':
         reset();
         break;
       case '+/-':
-        num2.value = '${-int.parse(num2.value)}'.toString();
+        ouput.value = '${-y}'.toString();
         break;
       case '-':
         result.value = '${x - y}';
@@ -29,26 +29,39 @@ class CalculatorController extends GetxController {
         result.value = '${x + y}';
         break;
       case '*':
+        if (x == 0) x = 1;
         result.value = '${x * y}';
         break;
       case '÷':
+        if (x == 0) {
+          x = y;
+          y = 1;
+        }
+        print('/ = ${x}, ${y}');
+        print(x / y);
         result.value = '${x / y}';
         break;
       case '=':
-        num2.value = result.value;
+        //ouput.value = result.value;
+        operand(operation.value);
         break;
     }
+    operation.value = o;
+    ouput.value = result.value;
   }
 
   addNumber(String number) {
-    if (num2.value == '0') {
-      return num2.value = number;
+    if (ouput.value == result.value) {
+      ouput.value = '0';
+    }
+    if (ouput.value == '0') {
+      return ouput.value = number;
     }
 
-    if (num2.value == '-0') {
-      return num2.value = '-$number';
+    if (ouput.value == '-0') {
+      return ouput.value = '-$number';
     }
 
-    return num2.value = num2.value + number;
+    return ouput.value = ouput.value + number;
   }
 }
--- calculator/lib/views/calculator_page.dart
+++ calculator/lib/views/calculator_page.dart
@@ -2,11 +2,183 @@ import 'package:calculator/controller/calculator_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class CalculatorPage extends GetView<CalculatorController> {
+class CalculatorPage extends StatelessWidget {
+  const CalculatorPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    final CalculatorController controller = Get.put(CalculatorController());
     return Scaffold(
-        appBar: AppBar(title: Text('CalculatorPage')),
-        body: SafeArea(child: Text('CalculatorController')));
+      appBar: AppBar(title: Text('CalculatorPage')),
+      body: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.all(20.0),
+            child: Obx(
+              () => Text(
+                '${controller.ouput}',
+                textAlign: TextAlign.right,
+                style: TextStyle(fontSize: 25),
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () {
+                      controller.operand('AC');
+                    },
+                    child: Text('AC')),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () {
+                      controller.operand('+/-');
+                    },
+                    child: Text('+/-')),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () {
+                      controller.operand('%');
+                    },
+                    child: Text('%')),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () {
+                      controller.operand('÷');
+                    },
+                    child: Text('÷')),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () {
+                      controller.addNumber('7');
+                    },
+                    child: Text('7')),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () {
+                      controller.addNumber('8');
+                    },
+                    child: Text('8')),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () {
+                      controller.addNumber('9');
+                    },
+                    child: Text('9')),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () {
+                      controller.operand("*");
+                    },
+                    child: Text('*')),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () {
+                      controller.addNumber('4');
+                    },
+                    child: Text('4')),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () {
+                      controller.addNumber('5');
+                    },
+                    child: Text('5')),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () {
+                      controller.addNumber('6');
+                    },
+                    child: Text('6')),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () {
+                      controller.operand("-");
+                    },
+                    child: Text('-')),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () {
+                      controller.addNumber('1');
+                    },
+                    child: Text('1')),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () {
+                      controller.addNumber('2');
+                    },
+                    child: Text('2')),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () {
+                      controller.addNumber('3');
+                    },
+                    child: Text('3')),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () {
+                      controller.operand('+');
+                    },
+                    child: Text('+')),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: ElevatedButton(
+                    onPressed: () {
+                      controller.addNumber('0');
+                    },
+                    child: Text('0')),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () {
+                      controller.operand(".");
+                    },
+                    child: Text('.')),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () {
+                      controller.operand("=");
+                    },
+                    child: Text('=')),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
