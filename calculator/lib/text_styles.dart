2a85445611f64c8b474f3dfa405c97ad86d1f59a
kopro-ov <koprodev@gmail.com>
2022-05-30 20:00:49 +0900

    Add text_styles

---
+++ calculator/lib/text_styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  static const dart_13 =
+      TextStyle(color: Colors.black, fontSize: 13, letterSpacing: 0.8);
+  static const dart_14 =
+      TextStyle(color: Colors.black, fontSize: 14, letterSpacing: 0.8);
+  static const dart_15 =
+      TextStyle(color: Colors.black, fontSize: 15, letterSpacing: 0.8);
+  static const dart_16 =
+      TextStyle(color: Colors.black, fontSize: 16, letterSpacing: 0.8);
+}
