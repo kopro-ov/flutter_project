4b369bbb85cfe28f87a02f5098e214673d2b6b2b
kopro-ov <koprodev@gmail.com>
2022-06-07 03:30:57 +0900

    Feat 머티리얼 타이머
    - WorkFlow
    1. timer_pciker_page 생성
    2. _selectTime 메소드로 showTimePicker
    3. 24시간 선택으로 변경

---
+++ basic_ex/lib/pages/time_picker_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class TimerPickerPage extends StatefulWidget {
+  @override
+  _TimerPickerPageState createState() => _TimerPickerPageState();
+}
+
+class _TimerPickerPageState extends State<TimerPickerPage> {
+  TimeOfDay _time = TimeOfDay(hour: 7, minute: 15);
+
+  void _selectTime() async {
+    final TimeOfDay? newTime = await showTimePicker(
+      context: context,
+      initialTime: _time,
+      builder: (BuildContext context, Widget? child) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
+          child: child!,
+        );
+      },
+    );
+    if (newTime != null) {
+      setState(() {
+        _time = newTime;
+      });
+    }
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: _selectTime,
+              child: Text('SELECT TIME'),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Selected time: ${_time.format(context)}',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
