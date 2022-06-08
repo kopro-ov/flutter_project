901070e05bb0e5e1bae9cf02872660df71929729
kopro-ov <koprodev@gmail.com>
2022-06-08 15:19:31 +0900

    Add 버튼3개 및 책 설명

--- book_list_sample/lib/screens/detail_screen.dart
+++ book_list_sample/lib/screens/detail_screen.dart
@@ -37,18 +37,66 @@ class DetailScreen extends StatelessWidget {
                   ],
                 ),
               ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.15,
+                padding: const EdgeInsets.all(10),
+                child: const Center(
+                  child: Icon(
+                    Icons.star,
+                    color: Colors.red,
+                  ),
+                ),
+              ),
             ],
           ),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.15,
-            padding: const EdgeInsets.all(10),
-            child: const Center(
-              child: Icon(
-                Icons.star,
-                color: Colors.red,
-              ),
-            ),
+          const Padding(
+            padding: EdgeInsets.all(3),
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: const [
+                  Icon(
+                    Icons.call,
+                    color: Colors.blue,
+                  ),
+                  Text(
+                    'Contact',
+                    style: TextStyle(color: Colors.blue),
+                  )
+                ],
+              ),
+              Column(
+                children: const [
+                  Icon(
+                    Icons.near_me,
+                    color: Colors.blue,
+                  ),
+                  Text(
+                    'Route',
+                    style: TextStyle(color: Colors.blue),
+                  )
+                ],
+              ),
+              Column(
+                children: const [
+                  Icon(
+                    Icons.save,
+                    color: Colors.blue,
+                  ),
+                  Text(
+                    'Save',
+                    style: TextStyle(color: Colors.blue),
+                  )
+                ],
+              ),
+              Container(
+                padding: const EdgeInsets.all(15),
+                child: const Text('머신 러닝과...'),
+              )
+            ],
+          )
         ],
       ),
     );
