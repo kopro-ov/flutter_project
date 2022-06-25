f3c079ffd0a7f4f04bda7386751035838d0c0ecb
kopro-ov <koprodev@gmail.com>
2022-06-25 21:16:16 +0900

    Feat 로그인 화면에서 로그인 버튼 클릭 시 목록 화면으로 이동
    Workflow
    1. setLogin 메서드 작업
    - 데이터 파일에 isLogin 키 값을 true
    2. 로그인 버튼 생성
    3. 로그인 버튼 onPressed일 때 setLogin 실행
    - setLogin 데이터를 받아오면 이전 화면을 날리고 ListScreen 이동

--- todo/lib/screens/login_screen.dart
+++ todo/lib/screens/login_screen.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:todo/screens/list_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
+  Future setLogin() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool("isLogin", true);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('로그인'),
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.0),
+          width: MediaQuery.of(context).size.width * 0.85,
+          child: ElevatedButton(
+            onPressed: () {
+              setLogin().then((_) {
+                Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => ListScreen()));
+              });
+            },
+            child: const Text('로그인 하세염'),
+          ),
+        ),
       ),
     );
   }
